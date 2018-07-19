2892e2f7748a3c7b534ad499511404bd82d402e2
Jakub Wlodek <jwlodek@cs.stonybrook.edu>
2018-07-19 15:05:34 -0400

    worked on writeInt32 function and callback function

--- adUVCApp/src/ADUVC.cpp
+++ adUVCApp/src/ADUVC.cpp
@@ -32,7 +32,6 @@
 
 static const char* driverName = "ADUVC";
 
-static void exitCallbackC(void *drvPvt);
 
 /*
  * External configuration function for ADUVC.
@@ -45,6 +44,18 @@ extern "C" int ADUVCConfig(const char* portName, int devIndex, int maxBuffers, s
     new ADUVC(portName, devIndex, maxBuffers, maxMemory, priority, stackSize);
     return(asynSuccess);
 }
+
+/*
+ * Callback function called when IOC is terminated.
+ * Deletes created object
+ * 
+ * @params: pPvt -> pointer to the ADUVC object created in ADUVCConfig
+ */
+static void exitCallbackC(void* pPvt){
+    ADUVC* pUVC = (ADUVC*) pPvt;
+    delete(pUVC);
+}
+
 
 /*
  * Function used to display UVC errors
@@ -62,9 +73,10 @@ void ADUVC::reportUVCError(uvc_error_t status, const char* functionName){
  * then the device is identified, then opened.
  * NOTE: this driver must have exclusive access to the device as per UVC standards.
  * 
+ * @params: serial number of device to connect to
  * @return: bool -> true if connection is successful, false if failed
  */
-bool ADUVC::connectToDeviceUVC(){
+bool ADUVC::connectToDeviceUVC(int serialNumber){
     static const char* functionName = "connectToDeviceUVC";
     deviceStatus = uvc_init(&pdeviceContext, NULL);
 
@@ -75,7 +87,7 @@ bool ADUVC::connectToDeviceUVC(){
     else{
         asynPrint(this->pasynUserSelf, ASYN_TRACE_ERROR, "%s::%s Initialized UVC context", driverName, functionName);
     }
-    deviceStatus = uvc_find_device(pdeviceContext, &pdevice, 0, 0, NULL);
+    deviceStatus = uvc_find_device(pdeviceContext, &pdevice, 0, 0, serialNumber);
     if(deviceStatus<0){
         reportUVCError(deviceStatus, functionName);
         return false;
@@ -109,6 +121,60 @@ void ADUVC::getDeviceInformation(){
     setStringParam(ADModel, modelName);
 }
 
+uvc_error_t ADUVC::acquireStart(){
+    //TODO
+}
+
+uvc_error_t ADUVC::acquireStop(){
+    //TODO
+}
+
+void ADUVC::newFrameCallback(uvc_frame_t* fram, void* ptr){
+    void* pData;
+    int aquiring;
+    NDArray* pArray;
+    NDDataType_t dataType = NDUInt16;
+    epicsTimeStamp currentTime;
+    static const char* functionName = "newFrameCallback";
+    asynPrint(this->pasynUserSelf, ASYN_TRACE_IO_DRIVER, "%s::%s Entering callback function\n", driverName, functionName);
+    this->lock();
+
+}
+
+asynStatus ADUVC::writeInt32(asynUser* pasynUser, epicsInt32 value){
+    
+    int function = pasynUser->reason;
+    int acquiring;
+    int status = asynSuccess;
+    static const char* functionName = "writeInt32";
+    getIntegerParam(ADAcquire, &acquiring);
+
+    
+    status = setIntegerParam(function, value);
+
+    if(function == ADAcquire){
+        if(value && !acquiring){
+            deviceStatus = acquireStart();
+            if(deviceStatus < 0) reportUVCError(deviceStatus, functionName);
+        }
+        if(!value && acquiring){
+            deviceStatus = acquireStop();
+            if(deviceStatus < 0) reportUVCError(deviceStatus, functionName);
+        }
+    }
+    else{
+        if (function < DEX_FIRST_PARAM) {
+            status = ADDriver::writeInt32(pasynUser, value);
+        }
+    }
+    callParamCallbacks();
+
+    if(status){
+        asynPrint(this->pasynUserSelf, ASYN_TRACE_ERROR, "%s::%s ERROR status=%d, function=%d, value=%d\n", driverName, functionName, status, function, value);
+    }
+    else asynPrint(this->pasynUserSelf, ASYN_TRACEIO_DRIVER, "%s::%s function=%d value=%d\n", driverName, functionName, function, value);
+}
+
 /*
  * Constructor for ADUVC driver. Most params are passed to the parent ADDriver constructor. 
  * Connects to the camera, then gets device information, and is ready to aquire images.
@@ -120,7 +186,7 @@ void ADUVC::getDeviceInformation(){
  * @params: priority -> what thread priority this driver will execute with
  * @params: stackSize -> size of the driver on the stack
  */
-ADUVC::ADUVC(const char* portName, int devIndex, int maxBuffers, size_t maxMemory, int priority, int stackSize)
+ADUVC::ADUVC(const char* portName, int serialNumber, int maxBuffers, size_t maxMemory, int priority, int stackSize)
     : ADDriver(portName, 1, (int)NUM_UVC_PARAMS, maxBuffers, maxMemory, asynEnumMask, asynEnumMask, ASYN_CANBLOCK, 1, priority, stackSize){
         
     int status = asynSuccess;
@@ -130,14 +196,25 @@ ADUVC::ADUVC(const char* portName, int devIndex, int maxBuffers, size_t maxMemor
     createParam(ADUVC_ReferenceCountString,         asynParamInt32,     ADUVC_ReferenceCount);
     createParam(ADUVC_SerialNumberString,           asynParamInt32,     ADUVC_SerialNumber);
 
-    bool connected = connectToDeviceUVC();
+    bool connected = connectToDeviceUVC(serialNumber);
     if(!connected){
-        asynPrint(this->pasynUserSelf, ASYN_TRACE_ERROR, "%s::%s Connection failed, abort", driverName, functionName);
+        asynPrint(this->pasynUserSelf, ASYN_TRACE_ERROR, "%s::%s Connection failed, abort\n", driverName, functionName);
     }
     else{
         getDeviceInformation();
     }
     
+}
+
+/*
+ * ADUVC destructor. Called by the exitCallbackC function when IOC is shut down
+ */
+ADUVC::~ADUVC(){
+    static const char* functionName = "~ADUVC";
+    asynPrint(this->pasynUserSelf, ASYN_TRACEIO_DRIVER, "%s::%s Calling all free functions for ADUVC\n", driverName, functionName);
+    uvc_free_device_descriptor(pdeviceInfo);
+    uvc_close(pdeviceHandle);
+    delete(pdevice);
 }
 
 
