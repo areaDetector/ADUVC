b8019ceeb5a6c6abe5f9e6668bf76b3dbe734489
Jakub Wlodek <jwlodek@cs.stonybrook.edu>
2018-07-25 14:05:12 -0400

    updated install uvc script

--- adUVCSupport/installlibuvc.sh
+++ adUVCSupport/installlibuvc.sh
@@ -7,3 +7,9 @@ cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make
 sudo make install
+
+mkdir ../../os
+mkdir ../../os/linux-x86_64
+cp libuvc.so ../../os/linux-x86_64/.
+cp libuvc.so.0.0.6 ../../os/linux-x86_64/.
+echo "Finished installing libuvc"
